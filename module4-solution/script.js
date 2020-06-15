07750d597db7d0df5cbb3bd8270c8d81c7c65e37
soumyaranjan-01 <48331344+soumyaranjan-01@users.noreply.github.com>
2020-06-15 20:44:59 +0530

    Add files via upload

---
+++ module4-solution/script.js
@@ -0,0 +1,8 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for(var i in names){
+  if(names[i].charAt(0).toLowerCase() == 'j'){
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
